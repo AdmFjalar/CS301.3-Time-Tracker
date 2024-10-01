828082c9239c408caeb67f4daed228d5cad0e03a
Dylan Green <dylan.green801@gmail.com>
2024-10-01 18:44:27 +1300

    made files and folder for frontend

---
+++ frontend/Timetracker.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Classprops from "./Classprops";
+import Functionprops from './Functionprops';
+
+class Timetracker extends React.Component {
+    render(){
+        return (
+            <div>
+                <Classprops name="Manager">
+                    <p>Child Component</p>
+                    </Classprops>
+                <Classprops name="Employer">
+                    <button>Click</button> 
+                    </Classprops>
+                <Classprops name="Employee"/>
+                <Functionprops name="Admin"></Functionprops>
+            </div>
+        );
+    }
+}
+
+export default Timetracker;
