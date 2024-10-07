c4fdf3a672c43ad2c2143d4ab9f76ad5bd0eacd3
Oliver Grönkrans <oliver@gronkrans.com>
2024-10-07 15:33:54 +1300

    Added util functions
    Added util functions to standardize response pipeline

--- backend/service/user/routes.go
+++ backend/service/user/routes.go
@@ -3,6 +3,8 @@ package user
 import (
 	"net/http"
 
+	"github.com/AdmFjalar/CS301.3-Time-Tracker/backend/types"
+	"github.com/AdmFjalar/CS301.3-Time-Tracker/backend/utils"
 	"github.com/gorilla/mux"
 )
 
@@ -23,5 +25,13 @@ func (h *Handler) handleLogin(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
+	// get JSON payload
+	var payload types.RegisterUserPayload
+	if err := utils.ParseJSON(r, payload); err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+	}
 
+	// check if the user exists
+
+	// if it doesn't we create the new user
 }
