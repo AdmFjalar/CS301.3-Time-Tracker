bad7adc5c2cac55db1106cf675b835b5182ec024
Dylan Green <dylan.green801@gmail.com>
2024-11-17 21:50:37 +1300

    front end refactoring
    - made login page look like the hi-fi prototype
    - fixed routing issues

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,32 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { AuthProvider } from './components/AuthContext'; // Import AuthProvider
-import Sidebar from './components/Sidebar';
-import UpcomingShifts from './components/UpcomingShifts';
 import HomePage from './components/HomePage';
-import Overview from './components/Overview';
 import WorkedTime from './components/WorkedTime';
 import Login from './components/Login';
 import Register from './components/Register';
 import Activate from './components/Activate';
+import AppLayout from './AppLayout'; // New component for layout
 import './App.css';
 
 const App = () => {
   return (
     <AuthProvider>
       <Router>
-        <div className="app-container">
-          <Sidebar />
-          <div className="main-content">
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/worked-time" element={<WorkedTime />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/confirm/:token" element={<Activate />} />
-            </Routes>
-          </div>
-        </div>
+        <Routes>
+          {/* Wrap all routes in the AppLayout which handles layout logic */}
+          <Route path="/" element={<AppLayout />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/worked-time" element={<WorkedTime />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/confirm/:token" element={<Activate />} />
+          </Route>
+          <Route path="/login" element={<Login />} /> {/* No Sidebar on login page */}
+        </Routes>
       </Router>
     </AuthProvider>
   );
 };
 
 export default App;
+
